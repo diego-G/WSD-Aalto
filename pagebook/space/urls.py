93fd9a60f43d2025fd596198f395ccb18f85157e
Xavi Magrinyà <xavi.mb89@hotmail.com>
2012-02-21 16:49:43 +0200

    more afternoon parties

--- pagebook/space/urls.py
+++ pagebook/space/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('space.views',
 
     url(r'^$', 'home', name='home_user'),
-    url(r'^create_album/', 'create', name='create_album'),
-    url(r'^delete_album/(?P<album>\d+)/', 'delete', name='delete_album'),
+    url(r'^create_album/', 'create_album', name='create_album'),
+    url(r'^delete_album/(?P<album>\d+)/', 'delete_album', name='delete_album'),
     
     #url(r'^(?P<name>\w+)/$', include('space.urls'), name='space'),
 
