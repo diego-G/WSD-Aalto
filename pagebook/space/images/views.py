35cf8ac01fc2742167928731762d3c7e6025e8f2
Diego Garcia <dijeygui@msn.com>
2012-02-29 21:33:52 +0200

    Upload File Done. Miss show images of DB

--- pagebook/space/images/views.py
+++ pagebook/space/images/views.py
@@ -7,39 +7,40 @@ from django.contrib.auth.models import User
 from django.core.context_processors import csrf
 from django.http import (HttpResponse, HttpResponseRedirect,
                           HttpResponseBadRequest)
+from form import ImageUploadForm
 
 from settings import MEDIA_ROOT
 
 def images(request, name):
     template = "space/images/images.html"
+    
     c = {}
     c.update(csrf(request))
     try:
         usr = User.objects.get(username=name)
     except usr.DoesNotExist:
         raise Http404
-    if request.user.is_authenticated():
-        if request.user==usr:            
-            return render_to_response(template, RequestContext(request,{
-                'user':request.user, 'owner':usr,
-            }))
+    
+    if request.method == 'POST':
+        form = ImageUploadForm(request.POST, request.FILES)
+        save_file(request.FILES['image'])
+        return HttpResponseRedirect(".")
+
+    else:
+        if request.user.is_authenticated():
+            if request.user==usr:  
+                form = ImageUploadForm()          
+                return render_to_response(template, RequestContext(request,{
+                    'user':request.user, 'owner':usr, 'form': form,  'images': MEDIA_ROOT+"/images/"
+                }))
+            else:
+                raise Http404
         else:
             raise Http404
-    else:
-        raise Http404
- 
-    if request['method'] == 'POST':
-        if 'file' in request.FILES:
-            file = request.FILES['file']
- 
-            filename = file['filename']
- 
-            fd = open('%s/%s' % (MEDIA_ROOT, filename), 'wb')
-            fd.write(file['content'])
-            fd.close()
- 
-            return http.HttpResponseRedirect(template)
-    else:
-        # display the form
-        form = ImageUploadForm()
-        return render_to_response(template, { 'form': form })
+
+def save_file(file, path=''):
+    filename = file._get_name()
+    fd = open('%s/%s' % (MEDIA_ROOT+"/images/", str(path) + str(filename)), 'wb')
+    for chunk in file.chunks():
+        fd.write(chunk)
+    fd.close()
