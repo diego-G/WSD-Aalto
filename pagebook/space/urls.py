7114df81949340627902bef672831802a9a9ae64
Xavi Magrinyà <xavi.mb89@hotmail.com>
2012-03-04 13:11:40 +0200

    jquery improved

--- pagebook/space/urls.py
+++ pagebook/space/urls.py
@@ -1,23 +1,21 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.views.static import *
 
-urlpatterns = patterns('',
-    url(r'^(?P<album>\d+)/(?P<page>\d+)/edit_page/$', include('space.editor.urls'), name= 'editor'),                  
-)
 
-urlpatterns += patterns('space.views',
 
-    url(r"^js/js_page.js", 'render_javascript', name='render_javascript'),
+urlpatterns = patterns('space.views',
+
+    #url(r"^js/js_page.js", 'render_javascript', name='render_javascript'),
     url(r'^$', 'home', name='home_user'),
     url(r'^create_album/$', 'create_album', name='create_album'),
-    url(r'^delete_album/(?P<album>\d+)/', 'delete_album', name='delete_album'),
+    url(r'^delete_album/(?P<album>\d+)/$', 'delete_album', name='delete_album'),
     url(r'^(?P<album>\d+)/$', 'show_album', name='show_album'),
-    url(r'^(?P<album>\d+)/create_page/', 'create_page', name='create_page'),
-    url(r'^(?P<album>\d+)/delete_page/(?P<page>\d+)/', 'delete_page', name='delete_page'),
-    #url(r'^(?P<name>\w+)/$', include('space.urls'), name='space'),
-    url(r'^(?P<album>\d+)/view/', 'pages', name='pages'),
-    url(r'^(?P<album>\d+)/(?P<page>\d+)/', 'render_page', name='render_page'),
-    
+    url(r'^(?P<album>\d+)/create_page/$', 'create_page', name='create_page'),
+    url(r'^(?P<album>\d+)/delete_page/(?P<page>\d+)/$', 'delete_page', name='delete_page'),
+    url(r'^(?P<album>\d+)/view/$', 'pages', name='pages'),
+    #url(r'^(?P<album>\d+)/(?P<page>\d+)/edit_page/$', include('space.editor.urls'), name= 'editor'),
+    url(r'^(?P<album>\d+)/(?P<page>\d+)/$', 'render_page', name='render_page'),
+    url(r'^(?P<album>\d+)/(?P<page>\d+)/edit_page/', include('space.editor.urls'), name= 'edit_page'),
     url(r'^images/', include('space.images.urls'), name='images'),
         
     url(r'^change_pass/$', 'change_pass', name='change_pass'),
