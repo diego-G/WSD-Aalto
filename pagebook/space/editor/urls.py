e83c0288b81922194e6261e4343fd58cd181290d
Diego Garcia <dijeygui@msn.com>
2012-03-04 15:51:28 +0200

    Editor.....

--- pagebook/space/editor/urls.py
+++ pagebook/space/editor/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.views.static import *
 
-urlpatterns = patterns('',
-    url(r'^$', 'edit_page', name='edit_page'),                 
+urlpatterns = patterns('space.editor.views',
+    url(r'^$', 'edit_page', name='edit_page'),
+    url(r'^choose_image/$', 'choose_image', name='choose_image'),            
 )
--- pagebook/space/editor/views.py
+++ pagebook/space/editor/views.py
@@ -1,5 +1,49 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from space.models import Album, Page, Image, AlbumForm, PageForm
+from django.core.context_processors import csrf
+
+from django.http import HttpResponse, HttpResponseRedirect
 from django.http import Http404
+from django.shortcuts import render_to_response
+from django.template import Context, RequestContext
 
+from settings import MEDIA_ROOT
+import os
 
+@login_required
 def edit_page(request, name, album, page ):
-    raise Http404
+    try:
+        usr = User.objects.get(username=name)
+    except User.DoesNotExist:
+        raise Http404
+    try:
+        album_ = Album.objects.get(user=usr, id=album)
+    except Album.DoesNotExist:
+        raise Http404
+    page_ = Page.objects.get(album=album_, number=page)
+    
+    path = MEDIA_ROOT+"/images/"+usr.username
+    listing = os.listdir(path)
+    
+    return render_to_response("space/editor/edit_page.html", RequestContext(request,{
+              'user':request.user, 'album': album_, 'page':page_, 'listing': listing
+    }))
+    
+def choose_image(request, name, album, page ):
+    try:
+        usr = User.objects.get(username=name)
+    except User.DoesNotExist:
+        raise Http404
+    try:
+        album_ = Album.objects.get(user=usr, id=album)
+    except Album.DoesNotExist:
+        raise Http404
+    page_ = Page.objects.filter(album=album_)
+    
+    path = MEDIA_ROOT+"/images/"+usr.username
+    listing = os.listdir(path)
+    
+    return render_to_response("space/editor/edit_page.html", RequestContext(request,{
+              'user':request.user, 'album': album_, 'page':page_, 'listing': listing
+    }))
