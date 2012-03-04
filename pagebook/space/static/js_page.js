faf771a8a9991043f8ac9c6ba3ab65a406300c63
Xavi Magrinyà <xavi.mb89@hotmail.com>
2012-03-04 13:55:03 +0200

    ajax works!

--- pagebook/space/static/js_page.js
+++ pagebook/space/static/js_page.js
@@ -11,5 +11,15 @@ $(function() {
         // Return false to prevent the default behavior of "click" (changing the page)
         return false;
     });
-
+	$("#next").live('click', function() {
+        // Take the target url from the link that was clicked
+        var url = $("#next a").attr("href");
+        
+        // Replace the contents of #tableContainer with data 
+        // that is dynamically from the link's target url
+        $("#dynamicContent").load(url);
+        
+        // Return false to prevent the default behavior of "click" (changing the page)
+        return false;
+    });
 } );
