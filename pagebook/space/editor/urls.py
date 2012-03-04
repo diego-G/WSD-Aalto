d7b6c25e0c9d84076fec83070154628959e17a42
Diego Garcia <dijeygui@msn.com>
2012-03-04 20:01:24 +0200

    Editor almost Ready

--- pagebook/space/editor/urls.py
+++ pagebook/space/editor/urls.py
@@ -3,5 +3,6 @@ from django.views.static import *
 
 urlpatterns = patterns('space.editor.views',
     url(r'^$', 'edit_page', name='edit_page'),
-    url(r'^choose_image/$', 'choose_image', name='choose_image'),            
+    url(r'^choose_image/(?P<nImage>\d+)/$', 'choose_image', name='choose_image'),
+    url(r'^asign_image/(?P<nImage>\d+)/(?P<file>\w+\.\w+)$', 'asign_image', name='asign_image'),                 
 )
